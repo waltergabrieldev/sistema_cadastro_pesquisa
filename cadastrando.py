502168e692a21e39701a62234bfb5f103d5b5123
Walter Gabriel <waltergabriel.dev@gmail.com>
2025-03-01 18:39:57 -0300

    base do cadastro e base de busca

--- cadastrando.py
+++ cadastrando.py
@@ -1,15 +1,39 @@
 import json
-import random
-def cadastro(dados):
+
+def cadastro(lista):
     dicionario_de_cadastrados = {}
-    id = random.randint(100000, 999999)
     with open("banco_de_dados.json", "r+") as file:
         file.seek(0)
         dicionario_de_cadastrados = json.loads(file.read())
-    dicionario_de_cadastrados[id] = dados
-    dicionario_de_cadastrados = json.dumps(dicionario_de_cadastrados)
+        id = int(list(dicionario_de_cadastrados.keys())[-1])
+        id = str(id + 1).zfill(6)
+    dicionario_de_cadastrados[id] = {"nome" : lista[0] , "cpf" : lista[1], "telefone" : lista[2]}
+    dicionario_de_cadastrados = json.dumps(dicionario_de_cadastrados, indent = 3)
     with open("banco_de_dados.json", 'w+') as file:
         file.write(dicionario_de_cadastrados)
+    print("cadastro finalizado com sucesso!")
 
+def coferindo_cpf_existentes(cpf):
+    with open("banco_de_dados.json", "r+") as file:
+        file.seek(0)
+        dicionario_de_cadastrados = json.loads(file.read())
 
+    verifica_cpf = [id for id, info in dicionario_de_cadastrados.items() if info["cpf"] == cpf]    
+    if verifica_cpf:
+        existente_no_sistema = False
+    elif not verifica_cpf :
+        existente_no_sistema = True
+    return existente_no_sistema
+    
+def coferindo_telefone_existentes(telefone):
+    with open("banco_de_dados.json", "r+") as file:
+        file.seek(0)
+        dicionario_de_cadastrados = json.loads(file.read())
 
+    verifica_telefone = [id for id, info in dicionario_de_cadastrados.items() if info["telefone"] == telefone]    
+    if verifica_telefone:
+        existente_no_sistema = False
+    elif not verifica_telefone :
+        existente_no_sistema = True
+    return existente_no_sistema
+    
---
+++ validar_cpf.py
@@ -0,0 +1,68 @@
+def verificacao(cpf):
+    
+    cpf = (cpf.replace('.','').replace('-',''))
+    
+    if cpf == cpf[0]*11:
+        
+        cpf_valido = False
+    else:   
+        
+        cpf_verificador = cpf[0:9]
+        calculo_multiplicacao = 0
+        
+        for contagem_regressiva, numeros in zip(range(10,1,-1), cpf_verificador) :
+            calculo_multiplicacao += int(numeros) * contagem_regressiva
+            
+        calculo_divisao = (calculo_multiplicacao % 11)
+        
+        if calculo_divisao < 2:
+            numeros_finais_1 = 0
+        else:
+            numeros_finais_1 = 11 - calculo_divisao
+            
+        cpf_verificador += str(numeros_finais_1)
+        calculo_multiplicacao = 0
+        calculo_divisao = 0
+        
+        for contagem_regressiva, numeros in zip(range(11,1,-1), cpf_verificador) :
+            calculo_multiplicacao += int(numeros) * contagem_regressiva
+            
+        calculo_divisao = (calculo_multiplicacao % 11)
+        
+        if calculo_divisao < 2:
+            numeros_finais_2 = 0
+        else:
+            numeros_finais_2 = 11 - calculo_divisao
+        cpf_verificador += str(numeros_finais_2)
+        
+        if cpf_verificador == cpf:
+            cpf_valido = True
+        else:
+            cpf_valido = False
+    
+    return cpf_valido
+
+def confirmacao_cpf_padrao():
+    indice_cpf = 0
+    while indice_cpf < 14:
+        #recebimento do cpf do usuario
+        cpf = input('cadastre um cpf(***.***.***-**): \n>>> ')
+        #verificando se o usuario nao burlou o recebimento 
+        if len(cpf) != 14:
+            print("Erro: O CPF deve ter exatamente 14 caracteres no formato ***.***.***-**")
+            continue
+        for char in cpf:
+            if indice_cpf == 3 or indice_cpf == 7:
+                if char != '.':
+                    print('digite o cpf dentro do padrao indicado!')
+                    break
+            elif indice_cpf == 11:
+                if char != '-':
+                    print('digite o cpf dentro do padrao indicado!')
+                    break      
+            else:
+                if not char.isnumeric():
+                    print('digite o cpf dentro do padrao indicado!')
+                    break
+            indice_cpf += 1
+    return(cpf)
